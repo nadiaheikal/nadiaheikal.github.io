2166b2f22e5380fe870fdbccecda5a2d61dc53bc
nadiaheikal <nah366@nyu.edu>
2022-03-30 15:48:23 -0400

    commit this

---
+++ Images4home/Button/button.js
@@ -0,0 +1,9 @@
+let change = document.getElementById("body")
+// Storing info in variable change (look at webpage and get the html element of body)
+let theTxt = document.querySelector("h1")
+theButton.addEventListener('click', clickbutton)
+function clickbutton() {
+    console.log("YOU CLICKED IT!!")
+    change.style.background = "orange";
+    theTxt.textContent = "Orange you glad you clicked!"
+}
